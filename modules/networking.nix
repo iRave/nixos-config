45ee78c5b66b04c5e49dae598cd0e5cac0d5608d
Till Schmidt <till.schmidt@tilltheend.de>
2025-06-26 21:10:32 +0200

    Enable SSH

--- modules/networking.nix
+++ modules/networking.nix
@@ -7,4 +7,9 @@
   time.timeZone = "Europe/Berlin";
   i18n.defaultLocale = "en_US.UTF-8";
   console.keyMap = "de";
+
+  services.openssh.enable = true;
+  services.openssh.permitRootLogin = "no";  # or "prohibit-password" for keys only
+  services.openssh.passwordAuthentication = true;  # or false if using SSH keys
+  networking.firewall.allowedTCPPorts = [ 22 ];
 }
