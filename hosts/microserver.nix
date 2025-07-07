e86f3e5002a7f9eb22f93e2fcb7ab5f5a4bd9890
Till Schmidt <till.schmidt@tilltheend.de>
2025-07-07 14:12:19 +0200

    Update Hassos

--- hosts/microserver.nix
+++ hosts/microserver.nix
@@ -2,21 +2,37 @@
 
 {
   imports = [];
-  
+
   networking.hostName = lib.mkForce "nixos-microserver";
-  
+
   nixpkgs.config.allowUnfree = true;
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  virtualisation.libvirtd.enable = true;
-  environment.systemPackages = with pkgs; [
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      package = pkgs.qemu_kvm;
+      runAsRoot = true;
+      swtpm.enable = true;
+      ovmf = {
+        enable = true;
+        packages = [(pkgs.OVMF.override {
+          secureBoot = true;
+          tpmSupport = true;
+        }).fd];
+      };
+    };
+  };
+   environment.systemPackages = with pkgs; [
     wget
     git
     qemu
     virt-manager
-    inputs.compose2nix.packages.x86_64-linux.default
+    compose2nix
+    neovim
+    btop
   ];
 
   system.stateVersion = "25.05";
--- modules/hassos.nix
+++ modules/hassos.nix
@@ -1,25 +1,54 @@
 { config, pkgs, lib, ... }:
 
 let
-  haImage = pkgs.fetchurl {
+  haCompressed = pkgs.fetchurl {
     url = "https://github.com/home-assistant/operating-system/releases/download/15.2/haos_ova-15.2.qcow2.xz";
+    hash = "sha256-bRgQe/X0GrSRjLJ/u+ztaIwXDoSGkDThPKOObZ1Uckk=";
   };
+
+  # Destination path for the unpacked image
+  imagePath = "/var/lib/libvirt/images/hassos.qcow2";
 in {
+  # Ensure libvirt directory exists
+  systemd.tmpfiles.rules = [
+    "d /var/lib/libvirt/images 0755 libvirt-qemu kvm -"
+  ];
+
+  # Unpack & stage the image at activation time
+  system.activationScripts.hassosImage = {
+    description = "Unpack and stage HassOS QCOW2 image";
+    text = ''
+      if [ ! -f "${imagePath}" ]; then
+        echo "Unpacking Home Assistant image..."
+        mkdir -p /var/lib/libvirt/images
+        cp ${haCompressed} /tmp/hassos.qcow2.xz
+        unxz /tmp/hassos.qcow2.xz
+        mv /tmp/hassos.qcow2 ${imagePath}
+        chown libvirt-qemu:kvm ${imagePath}
+        chmod 644 ${imagePath}
+      else
+        echo "HassOS image already present."
+      fi
+    '';
+  };
+
+  # Auto-define and start the VM
   systemd.services.hassos-vm = {
-    description = "Home Assistant VM";
+    description = "Home Assistant OS VM";
     after = [ "libvirtd.service" ];
     wantedBy = [ "multi-user.target" ];
     serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
       ExecStart = ''
+        ${pkgs.libvirt}/bin/virsh undefine hassos || true
         ${pkgs.libvirt}/bin/virsh define /etc/libvirt/hassos.xml
         ${pkgs.libvirt}/bin/virsh start hassos
       '';
-      Type = "oneshot";
-      RemainAfterExit = true;
     };
   };
 
-  # Add HassOS libvirt XML template to /etc/libvirt
+  # Define the VM via libvirt XML (stored in Nix)
   environment.etc."libvirt/hassos.xml".text = ''
     <domain type='kvm'>
       <name>hassos</name>
@@ -30,7 +59,8 @@ in {
       </os>
       <devices>
         <disk type='file' device='disk'>
-          <source file='${haImage}'/>
+          <driver name='qemu' type='qcow2'/>
+          <source file='${imagePath}'/>
           <target dev='vda' bus='virtio'/>
         </disk>
         <interface type='network'>
